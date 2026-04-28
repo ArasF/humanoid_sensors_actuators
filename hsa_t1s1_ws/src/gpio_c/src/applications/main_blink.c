dd390322165adaa08108a9ff147edb401a4f9c47
ercankacmaz <ercankacmaz870870@gmail.com>
2026-04-28 09:31:27 +0200

    T.3.7 main_blink is implemented

--- hsa_t1s1_ws/src/gpio_c/src/applications/main_blink.c
+++ hsa_t1s1_ws/src/gpio_c/src/applications/main_blink.c
@@ -5,9 +5,19 @@
 #include <util/delay.h>
 
 
-int main (void)
+int main(void)
 {
+    DDRC |= (1 << PC0);
 
-    // Should never be reached
+    while (1) {
+        PORTC |= (1 << PC0);
+        _delay_ms(1000);
+
+        PORTC &= ~(1 << PC0);
+        _delay_ms(1000);
+    }
+
     return 0;
 }
+
+
