0dc9c5d24fdec154f538dd0420003d71c5a766f6
ercankacmaz <ercankacmaz870870@gmail.com>
2026-04-28 11:05:14 +0200

    T.4.2 main_uart_echo is implemented

--- hsa_t1s2_ws/src/uart/src/applications/main_uart_hello_world.c
+++ hsa_t1s2_ws/src/uart/src/applications/main_uart_hello_world.c
@@ -1,13 +1,44 @@
-// Get register definitions with auto complete
-#include <atmega32/io.h>
+#define F_CPU 1000000UL
 
-// For delay functions: F_CPU has to be defined
+#include <atmega32/io.h>
 #include <util/delay.h>
 
-
-int main (void)
+static void uart_init(void)
 {
+    UCSRA = 0x00;
+    UBRRH = 0x00;
+    UBRRL = 0x00;
+    UCSRB = (1 << RXEN) | (1 << TXEN);
+    UCSRC = (1 << URSEL) | (1 << UCSZ1) | (1 << UCSZ0);
+}
 
-    // Should never be reached
+static void uart_send_blocking(unsigned char value)
+{
+    while ((UCSRA & (1 << UDRE)) == 0)
+    {
+    }
+
+    UDR = value;
+}
+
+static void uart_send_string(const char *text)
+{
+    while (*text != '\0')
+    {
+        uart_send_blocking((unsigned char)*text);
+        text++;
+    }
+}
+
+int main(void)
+{
+    uart_init();
+
+    while (1)
+    {
+        uart_send_string("Hello world!\n");
+        _delay_ms(1000);
+    }
+
     return 0;
 }
